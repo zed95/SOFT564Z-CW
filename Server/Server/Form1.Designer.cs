4164f6cad80a7c1471b6789dae1430b57c28854e
zed95 <zbigniew95@outlook.com>
2021-01-05 15:59:52 +0000

    Changed Folder and filenames for the server

---
+++ Server/Server/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace Server
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.StartServerBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // StartServerBtn
+            // 
+            this.StartServerBtn.Location = new System.Drawing.Point(12, 12);
+            this.StartServerBtn.Name = "StartServerBtn";
+            this.StartServerBtn.Size = new System.Drawing.Size(171, 63);
+            this.StartServerBtn.TabIndex = 0;
+            this.StartServerBtn.Text = "Start Server";
+            this.StartServerBtn.UseVisualStyleBackColor = true;
+            this.StartServerBtn.Click += new System.EventHandler(this.StartServerBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(199, 86);
+            this.Controls.Add(this.StartServerBtn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button StartServerBtn;
+    }
+}
+
---
+++ Server/Server/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.IO;
+using System.Net;
+using System.Threading;
+
+using System.Runtime.InteropServices;
+
+namespace Server
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void StartServerBtn_Click(object sender, EventArgs e)
+        {
+            //initialise the server when the button is clicked.
+            Server.initServer();
+            StartServerBtn.Enabled = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
